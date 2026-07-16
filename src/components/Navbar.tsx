76b0805ceefb7a230186ac76aaeea10ed63711a0
Samvedha Basireddy <sssb@uw.edu>
2026-07-15 17:45:05 -0700

    Styled initial version of Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,23 @@
 export default function Navbar() {
     return (
-        <nav>
-            <h2>College Planner</h2>
+        <nav className="flex justify-between bg-indigo-50 items-center px-8 py-4">
+            <h2 className="py-4 px-2 text-4xl font-sans text-sky-600"
+                style={{ fontFamily: "Merriweather, sans-serif" }}>
+                College Planner
+            </h2>
 
-            <ul>
+            <ul className="flex space-x-6"
+                style={{ fontFamily: "Poppins, sans-serif" }}>
                 <li>Features</li>
                 <li>About</li>
-                <li>Login/Sign-Up</li>
+                <li>Create Account</li>
+                
             </ul>
+
+            <button className="text-gray px-4 py-2 rounded-lg hover:text-green-600"
+                    style={{ fontFamily: "Poppins, sans-serif" }}>
+                Login/Sign-up
+            </button>
 
         </nav>
     );
