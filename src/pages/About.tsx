450ee1e39500128cd996baae43e22fe7233e413d
Samvedha Basireddy <sssb@uw.edu>
2026-07-18 18:22:44 -0700

    Finished basic layout of the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,22 @@
+import Navbar from '../components/Navbar';
+
 function About() {
     return (
         <div>
-            <h1>About</h1>
+            <Navbar />
+            <h1 className="px-8 py-10 text-4xl font-semibold"
+                style={{ fontFamily: "Poppins, sans-serif" }}>
+                About{" "}
+                <span className="text-5xl text-cyan-800"
+                      style={{ fontFamily: "Merriweather, sans-serif" }}>
+                    College Planner
+                </span>
+            </h1>
+
+            <p className="px-8">
+                My goal is to provide high school students with a tool to store all of the necessary information needed for college apps in one place, that is simple to navigate.
+            </p> 
+
         </div>
     );
 }
