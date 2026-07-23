d23652abe9362092f5a87e82020b8dfaee4b87cb
Samvedha Basireddy <sssb@uw.edu>
2026-07-22 21:30:34 -0700

    Added colored progress dots next to each college

--- src/components/CollegeCard.tsx
+++ src/components/CollegeCard.tsx
@@ -1,19 +1,34 @@
 export default function CollegeCard({ category, colleges }) {
     return (
-        <div className="w-72 min-h-96 bg-white rounded-2xl shadow-md p-6">
+        <div className="w-72 min-h-80 bg-indigo-100 rounded-2xl shadow-md p-6">
             <h2 className="text-2xl font-semibold text-center text-blue-700">
                 {category}
             </h2>
 
-            <div className="mt-6">
-                {colleges.map((college) =>(
-                    <p key={college.id}>
-                        {college.name}
-                    </p>
+            <div className="mt-6 h-50 overflow-y-auto border border-indigo-100 rounded-lg p-2 space-y-2">
+                {colleges.map((college) => (
+                    <div
+                        key={college.id}
+                        className="flex items-center justify-between p-2 rounded-md hover:bg-indigo-50"
+                    >
+                        <p>
+                            {college.name}
+                        </p>
+
+                        <div
+                            className={`w-3 h-3 rounded-full ${
+                                college.progress === "complete"
+                                    ? "bg-green-500"
+                                    : college.progress === "in-progress"
+                                    ? "bg-yellow-500"
+                                    : college.progress === "not-started"
+                                    ? "bg-red-500"
+                                    : "bg-red-500"
+                            }`}
+                        />
+                    </div>
                 ))}
             </div>
-
         </div>
-
     );
 }
--- src/pages/Colleges.tsx
+++ src/pages/Colleges.tsx
@@ -3,25 +3,25 @@ import CollegeCard from '../components/CollegeCard';
 
 export default function Colleges() {
     const safetyColleges = [
-        { id: 1, name: "University of Oregon" },
-        { id: 2, name: "University of Arizona" }
+        { id: 1, name: "University of Oregon", progress: "complete" },
+        { id: 2, name: "University of Arizona", progress: "in-progress" }
     ];
 
     const targetColleges = [
-        { id: 3, name:"University of Washington Seattle"},
-        { id: 4, name:"Purdue University"}
+        { id: 3, name:"University of Washington Seattle", progress: "not started"},
+        { id: 4, name:"Purdue University", progress: "in-progress"}
     ];
 
     const reachColleges = [
-        { id: 5, name: "Stanford University"},
-        { id: 6, name: "University of Virginia"}
+        { id: 5, name: "Stanford University", progress: "complete"},
+        { id: 6, name: "University of Virginia", progress: "not started"}
     ]
 
     return(
         <div>
             <Navbar />
 
-            <div className="flex justify-center gap-8">
+            <div className="flex justify-center gap-8 py-18">
                 <CollegeCard
                     category="Safety"
                     colleges={safetyColleges}
